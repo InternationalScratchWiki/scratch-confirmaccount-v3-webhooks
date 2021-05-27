8c2e0d8224a9699a7d0aec1b008a518a5e967b24
Jacob G <jacob-x@jvvgindustries.com>
2021-05-27 16:39:24 +0100

    first commit - created basic skeleton

---
+++ src/ScratchConfirmAccountWebhooksHooks.php
@@ -0,0 +1,14 @@
+<?php
+class ScratchConfirmAccountWebhooksHooks {
+	public static function onAccountRequestAction($accountRequest, $action, $actioningUsername, $comment) {
+		
+	}
+	
+	public static function onCreateAccount($accountRequest, $actioningUsername) {
+		
+	}
+	
+	public static function onAccountRequestSubmitted($username, $requestNotes) {
+		
+	}
+}
